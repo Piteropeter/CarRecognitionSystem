c4bcffe6fe0d9d9c400cb1cdc8e9932c5e55daae
Piotr Przybył <piteropeter@gmail.com>
2020-01-22 09:20:08 +0100

    Recognize app refactor

--- Recognize.py
+++ Recognize.py
@@ -1,46 +1,49 @@
-import ast
+import cv2
 import json
 import pickle
-import cv2
+import argparse
 from keras.models import load_model
 import tensorflow as tf
 from zipfile import ZipFile
 
 tf.get_logger().setLevel('FATAL')
 
-zip_path = "./network.zip"
-with ZipFile(zip_path, 'r') as zip_file:
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument("-m", "--model", type=str, default="model.zip", help="Path to generated zip file")
+argument_parser.add_argument("-i", "--image", type=str, required=True,
+                             default="VMMRdb/mini_austin_1975/1972 AUSTIN MINI COOPER_00C0C_3QPt1U5KBh5_600x450.jpg",
+                             help="Path to image to recognize")
+args = vars(argument_parser.parse_args())
+
+with ZipFile(args['model'], 'r') as zip_file:
     zip_file.extractall()
 
 file = open("./info.json", "r")
 data = file.read()
 data = json.loads(data)
 print(data)
-IMAGE_SIZE = data['IMAGE_SIZE']
+image_size = data['IMAGE_SIZE']
 
-image = cv2.imread("VMMRdb/mini_austin_1975/1972 AUSTIN MINI COOPER_00C0C_3QPt1U5KBh5_600x450.jpg")
+image = cv2.imread(args['image'])
 output = image.copy()
-image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
+image = cv2.resize(image, (image_size, image_size))
 image = image.astype("float") / 255.0
-image = image.reshape((1, IMAGE_SIZE, IMAGE_SIZE, 3))
+image = image.reshape((1, image_size, image_size, 3))
 
-print("[INFO] loading network and label binarizer...")
-# model = load_model("trained_models/network5m50b32s64augON")
+print("[INFO] Loading network and label binarizer...")
 model = load_model("./model")
-# lb = pickle.loads(open("trained_models/label_binarizer5m50b32s64augON", "rb").read())
 lb = pickle.loads(open("./label_binarizer", "rb").read())
-# make a prediction on the image
 
-preds = model.predict(image)
+predictions = model.predict(image)
 
-for i, pred in enumerate(preds[0]):
-    text = "{}: {:.2f}%".format(lb.classes_[i], preds[0][i] * 100)
+for i, pred in enumerate(predictions[0]):
+    text = "{}: {:.2f}%".format(lb.classes_[i], predictions[0][i] * 100)
     print(text)
 
 # find the class label index with the largest corresponding probability
 # draw the class label + probability on the output image
-best_recognition = preds.argmax(axis=1)[0]
-text = "{}: {:.2f}%".format(lb.classes_[best_recognition], preds[0][best_recognition] * 100)
+best_recognition = predictions.argmax(axis=1)[0]
+text = "{}: {:.2f}%".format(lb.classes_[best_recognition], predictions[0][best_recognition] * 100)
 cv2.putText(output, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 # show the output image
